e47fbd69b1b52702a4cc742326f35f950976cea5
dyep49 <dyep49@gmail.com>
2015-06-21 16:45:07 -0400

    cleans up temporary branches

--- view.js
+++ view.js
@@ -2,55 +2,84 @@ const phantom = require('phantom');
 const NodeGit = require('nodegit');
 const path = require('path');
 const Q = require('q');
+const argv = require('optimist').argv;
 
-var pagePath = 'file:///home/dylan/Projects/example-commit-viewer/index.html';
+const pathToRepo = path.resolve(argv.i);
+const pagePath = 'file://' + path.resolve(argv.f);
+const outputFolder = path.resolve(argv.o);
 
 var eventEmitter;
 var commitArray = [];
-
-var pathToRepo = path.resolve('../example-commit-viewer');
-
 var repository;
 
-NodeGit.Repository.open(pathToRepo).then(function(repo) {
-  repository = repo;
-  repo.checkoutBranch('gh-pages')
-  return repo.getBranchCommit('master');
-}).then(function(commit) {
-  eventEmitter = commit.history();
+var branches = [];
 
+function main() {
+  getMasterCommit(pathToRepo)
+  .then(buildCommitHistory)
+  .done(handleErrors);
+}
+
+function cleanupBranches() {
+  console.log('cleaning up');
+  repository.checkoutBranch('master');
+  branches.forEach(function(branch) {
+    var result = NodeGit.Branch.delete(branch);
+    console.log(result);
+  })
+}
+
+function handleErrors(err) {
+  if(err) console.log(err);
+  console.log('done');
+}
+
+function buildCommitHistory(commit) {
+  var eventEmitter = commit.history();
   eventEmitter.on('end', function(commits) {
-    commits.reduce(function(soFar, c) {
-      return soFar.then(function() {
-        return createBranchAndScreenshot(c, repository)
-      });
-    }, Q()).done(function() {
-      console.log('donezo')
-    })
-
-
-  });
-
-  eventEmitter.on('error', function(error) {
-    console.log(error);
-  });
+    buildScreenshotChain(commits)
+    .then(console.log);
+  })
+  eventEmitter.on('err', console.log);
 
   eventEmitter.start();
-}).done(function(e) {
-  console.log('done');
-  console.log(e);
-})
+}
 
-// takeScreenshot(pagePath).then(function() {
-//   console.log(done);
-// })
-function createBranchAndScreenshot(commit, repository) {
-  var branchName = commit.id().toString() + Date.now()
+function buildScreenshotChain(commits) {
+  return commits.reduce(function(soFar, commit) {
+    console.log('a');
+    return soFar.then(function() {
+      return createBranchAndScreenshot(commit);
+    });
+  }, Q()).done(function() {
+    cleanupBranches();
+    console.log('tests')
+  })
+}
+
+function getMasterCommit(pathToRepo) {
+  return NodeGit.Repository.open(pathToRepo).then(function(repo) {
+    repository = repo;
+    return repo.getBranchCommit('master');
+  })
+}
+
+function createBranchAndScreenshot(commit) {
+  var branchName = 'temporary' + Date.now();
   return NodeGit.Branch.create(repository, branchName, commit, 1, repository.defaultSignature(), "Created branch")
   .then(function(reference) {
-    repository.checkoutBranch(branchName, {})
-    return takeScreenshot(pagePath)
+    branches.push(reference);
+    repository.checkoutBranch(branchName)
+    return takeScreenshot(pagePath).then(function() {
+      repository.checkoutBranch('master');
+      var num = NodeGit.Branch.delete(reference);
+      console.log(num);
+    })
   });
+}
+
+function test(a) {
+  console.log('test', a);
 }
 
 
@@ -61,7 +90,16 @@ function takeScreenshot(filePath) {
     ph.createPage(function(page) {
       page.open(pagePath, function(status) {
         var time = new Date().getTime();
-        page.render('test' + time + '.png');
+        
+        page.evaluate(function() {
+          var style = document.createElement('style'),
+              text = document.createTextNode('body { background: #fff }');
+          style.setAttribute('type', 'text/css');
+          style.appendChild(text);
+          document.head.insertBefore(style, document.head.firstChild);
+        });
+
+        page.render(outputFolder + '/test' + time + '.png');
         ph.exit();
         deferred.resolve();
       })
@@ -72,6 +110,6 @@ function takeScreenshot(filePath) {
 }
 
 
-
+main();
 
 
