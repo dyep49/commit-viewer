cf9c87fe926a5d40a6cf682265e46a63069146dd
dyep49 <dyep49@gmail.com>
2015-07-03 21:49:07 -0400

    promise refactor

--- view.js
+++ view.js
@@ -1,7 +1,11 @@
+//example 
+//node view.js -i ./../example-commit-viewer -f ./../example-commit-viewer/index.html -o ./example/
+
 const phantom = require('phantom');
 const NodeGit = require('nodegit');
 const path = require('path');
 const Q = require('q');
+const _ = require('underscore');
 const argv = require('optimist').argv;
 
 const pathToRepo = path.resolve(argv.i);
@@ -11,77 +15,76 @@ const outputFolder = path.resolve(argv.o);
 var eventEmitter;
 var commitArray = [];
 var repository;
+var branches = []
 
-var branches = [];
 
-function main() {
-  getMasterCommit(pathToRepo)
-  .then(buildCommitHistory)
-  .done(handleErrors);
-}
+NodeGit.Repository.open(pathToRepo)
+.then(getMasterCommit)
+.then(getCommits)
+.then(buildScreenshotChain)
+.then(deleteBranches)
+.done(console.log)
 
-function cleanupBranches() {
-  console.log('cleaning up');
-  repository.checkoutBranch('master');
-  branches.forEach(function(branch) {
-    var result = NodeGit.Branch.delete(branch);
-    console.log(result);
-  })
-}
-
-function handleErrors(err) {
-  if(err) console.log(err);
-  console.log('done');
-}
-
-function buildCommitHistory(commit) {
-  var eventEmitter = commit.history();
+//Switch to master branch
+  //Get a list of commits
+    //For each commit
+      //Create a branch pointed at that commit
+        //Checkout that branch
+          //Take screenshot
+            //Delete branch
+function getCommits(masterCommit) {
+  var deferred = Q.defer();
+  var eventEmitter = masterCommit.history();
+  
   eventEmitter.on('end', function(commits) {
-    buildScreenshotChain(commits)
-    .then(console.log);
+    deferred.resolve(commits);
   })
-  eventEmitter.on('err', console.log);
+
+  eventEmitter.on('err', function() {
+    deferred.reject();
+  })
 
   eventEmitter.start();
+
+  return deferred.promise;
 }
+
+
+function deleteBranches(reference) {
+  repository.checkoutBranch('master');
+  branches.forEach(function(branch) {
+    var status = NodeGit.Branch.delete(branch);
+  })
+}
+
 
 function buildScreenshotChain(commits) {
   return commits.reduce(function(soFar, commit) {
-    console.log('a');
     return soFar.then(function() {
-      return createBranchAndScreenshot(commit);
+      var ref;
+      return createBranch(commit)
+             .then(function(reference) {
+                ref = reference;
+                branches.push(reference)
+                repository.checkoutBranch(reference);
+                return reference
+              })
+             .then(function(reference) {
+               return takeScreenshot();
+             })
     });
-  }, Q()).done(function() {
-    cleanupBranches();
-    console.log('tests')
-  })
+  }, Q())
 }
 
-function getMasterCommit(pathToRepo) {
-  return NodeGit.Repository.open(pathToRepo).then(function(repo) {
-    repository = repo;
-    return repo.getBranchCommit('master');
-  })
+function getMasterCommit(repo) {
+  repository = repo;
+  return repo.getBranchCommit('master');
 }
 
-function createBranchAndScreenshot(commit) {
+function createBranch(commit) {
   var branchName = 'temporary' + Date.now();
   return NodeGit.Branch.create(repository, branchName, commit, 1, repository.defaultSignature(), "Created branch")
-  .then(function(reference) {
-    branches.push(reference);
-    repository.checkoutBranch(branchName)
-    return takeScreenshot(pagePath).then(function() {
-      repository.checkoutBranch('master');
-      var num = NodeGit.Branch.delete(reference);
-      console.log(num);
-    })
-  });
 }
-
-function test(a) {
-  console.log('test', a);
-}
-
 
 function takeScreenshot(filePath) {
   var deferred = Q.defer();
@@ -109,7 +112,5 @@ function takeScreenshot(filePath) {
   return deferred.promise;
 }
 
-
-main();
 
 
